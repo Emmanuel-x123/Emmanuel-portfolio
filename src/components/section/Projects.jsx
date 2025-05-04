526606a80b1f55732d33822de6d89208dc96021a
emmanuel-x123 <esongz456@gmail.com>
2025-05-04 13:37:16 -0700

    made changes

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -104,6 +104,25 @@ export const Projects = () => {
                         className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project </a>
                     </div>
                 </div>
+                <div className="p-6 rounded-xl border border-gray/10 hover:-translate-y-1 hover:border-blue-500/30
+                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                    <h3 className="text-gray-400 text-xl font-bold mb-2">IQbase</h3>
+                    <p className="text-gray-400">IQbase is a learning management system where users can get courses and also become educators.</p>
+                    <div>
+                        {["React",].map((tech, key) => (
+                            <span
+                                 key={key}
+                                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                 >
+                                  {tech}
+                             </span>
+                        ))}
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <a href="https://iqbase.netlify.app/"
+                        className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project </a>
+                    </div>
+                </div>
            </div>
          </div>
          </RevealOnScroll>
