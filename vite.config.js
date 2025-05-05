403157db6fa1dee4ff6bd2ff885be0de8a841989
emmanuel-x123 <esongz456@gmail.com>
2025-05-05 04:42:55 -0700

    added a file

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
+    postcss: 'postcss.config.js',
   },
 });
-
