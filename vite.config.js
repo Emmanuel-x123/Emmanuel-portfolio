8614d472e365f06c74a964b942201f8863bb95bd
emmanuel-x123 <esongz456@gmail.com>
2025-05-05 10:23:49 -0700

    added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'build' // Change from default 'dist' to 'build'
   }
